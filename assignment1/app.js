1d346355e6f03372c37968292d723faf12b49dd6
Mankar <mankar91@gmail.com>
2017-02-18 23:48:57 -0500

    Assignment 1

---
+++ assignment1/app.js
@@ -0,0 +1,36 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+
+  $scope.dishes = "";
+
+  $scope.checkTooMuch = function () {
+
+    var dishNumber = countDishes($scope.dishes);
+    $scope.color = "green";
+
+    if (dishNumber == 0) {
+        $scope.message = "Please enter data first";
+    } else if (dishNumber <= 3) {
+        $scope.message = "Enjoy!";
+    } else {
+        $scope.message = "Too Much!";
+    }
+
+  };
+
+  function countDishes (dishes) {
+    if (dishes.length == 0) { return 0; }
+    return dishes.split(",").length;
+  }
+
+
+}
+
+})();
